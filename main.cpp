84988e33bd0b1e572aeb7f5dbfff84ffecd22be5
GerdaMiseviciute <gerdamis7@gmail.com>
2026-05-31 18:55:09 +0300

    Cross reference lentelė

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <set>
 #include <iomanip>
 #include <map>
 #include <algorithm>
@@ -18,6 +19,9 @@ using std::map;
 using std::istringstream;
 using std::cout;
 using std::endl;
+using std::set;
+using std::left;
+using std::setw;
 
 bool hasDigit(const std::string& s) {
     return std::any_of(s.begin(), s.end(), [](unsigned char ch) { return std::isdigit(ch); });
@@ -63,8 +67,11 @@ int main()
 {
     ifstream fd("tekstas.txt");
     ofstream fr("zodziai.txt");
+    ofstream fr1("cross_reference_lentele.txt");
     map<string, int>skirtingi_zodziai;
+    map<string, set<int>>zodziai_ir_eilutes;
     string eilute;
+    int eilutes_sk=1;
     while(getline(fd, eilute))
     {
         if(eilute.back()=='.')
@@ -74,14 +81,35 @@ int main()
         while(zodziai>>zodis)
         {
             if(!istrinti_zenklus(zodis).empty())
+            {
                 skirtingi_zodziai[didinti_pirma_raide(istrinti_zenklus(zodis))]++;
+                zodziai_ir_eilutes[didinti_pirma_raide(istrinti_zenklus(zodis))].insert(eilutes_sk);
+            }
         }
+        eilutes_sk++;
     }
 
     for(auto x: skirtingi_zodziai)
     {
         if(x.second>1)
             fr<<x.first<<" "<<x.second<<endl;
+    }
+
+    for(auto x : zodziai_ir_eilutes)
+    {
+        if(x.second.size()>1)
+        {
+            fr1<<x.first<<": ";
+            bool pirmas = true;
+            for(auto y : x.second)
+            {
+                if(!pirmas)
+                    fr1<<", ";
+                fr1<<y;
+                pirmas=false;
+            }
+            fr1<<endl;
+        }
     }
     return 0;
 }
